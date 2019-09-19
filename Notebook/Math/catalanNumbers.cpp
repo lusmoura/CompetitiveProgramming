b6ad0b1d78df385e3099e792925fe953fd062ee2
lusmoura <luisamoura@usp.br>
2019-09-19 17:26:52 -0300

    adding catalan numbers

---
+++ Notebook/Math/catalanNumbers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long int binomialCoeff(int n, int k) {
+	long long int ans = 1;
+
+	// c(n, k) = c(n, n-k)
+	if (k > n-k) k = n-k;
+
+	for (int i = 0; i < k; i++) {
+		ans *= (n-i);
+		ans /= (i+1);
+	}
+
+	return ans;
+}
+
+long long int catalan(int n) {
+	long long c = binomialCoeff(2*n, n);
+	return c/(n+1);
+}
+
+int main() {
+	int n; cin >> n;
+	cout << catalan(n) << '\n';
+
+	return 0;
+}
